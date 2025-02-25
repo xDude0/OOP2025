9dfbb8210f752b970654fefc5a2dc89dc7bc36f2
xDude0 <74557612+xDude0@users.noreply.github.com>
2025-02-25 19:33:59 +0200

    Add files via upload

---
+++ Lab 1/Pb1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int f(char *numar)
+{
+	int rez = 0;
+	int i = 0;
+	for (i = 0; numar[i] != '\0'; i++) {
+		rez= rez * 10 + (numar[i] - '0');
+	}
+
+	return rez;
+}
+
+int main()
+{
+	FILE *file;
+	int s = 0;
+	char nr[100];
+	if (fopen_s(&file, "TextFile1.txt", "r") != 0) 
+	{
+		printf_s("Error\n");
+		return 1;
+	}
+
+	while (fgets(nr, sizeof(nr), file))
+	{
+		s = s + f(nr);
+	}
+	fclose(file);
+
+	printf_s("%d\n", s);
+}
